f8dd11a87dbe49ed56dfd5f4d307dd41a93514ff
sirotovAB <sirotov@gmail.com>
2024-02-24 16:40:09 +0100

    up divkit version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,8 +45,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.yandex.div:div:24.3.0")
-    implementation("com.yandex.div:div-core:24.3.0")
+    implementation("com.yandex.div:div:29.7.0")
+    implementation("com.yandex.div:div-core:29.7.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
 
     testImplementation("junit:junit:4.13.2")
